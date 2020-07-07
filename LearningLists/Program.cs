7e8cd1552cacabef1725bed7236cbfddd12d6636
marco villanueva <villanuevamarco98@gmail.com>
2020-07-06 20:38:58 -0600

    Add project files.

---
+++ LearningLists/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> customers = new List<String>();
+
+            customers.Add("hoo");
+            customers.Add("ha");
+            customers.Add("he");
+
+            Console.WriteLine(customers.Count);
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+            Console.WriteLine(customers[1]);
+        }
+    }
+}
